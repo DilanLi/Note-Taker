25e5286d5f1d18b195869ddb1eab1c5bf19078f8
Dilan Li <dilanli0706@gmail.com>
2020-08-06 23:07:17 -0600

    added in code to deploy on heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,13 +4,18 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 //routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
